a6626da3e78d52a7675c74f766ea1a80f39f3fc5
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-01 21:41:47 -0500

    Modificado App.tsx: incorparando la vista HomeSreen en el enrutamiento de la aplicacion

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {HomeScreen} from '@/views'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen options={{headerShown:false}}name='Home' component={HomeScreen} />
+    </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
