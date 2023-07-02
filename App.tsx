e4250070371214c8c5676bc39f3b09b0a9a5e4d5
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-01 21:55:01 -0500

    Incorporando las vistas al enrutamiento del proyecto

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { BaseNavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {HomeScreen} from '@/views'
+import { HomeScreen, LoginScreen, SignUpScreen, WelcomeSreen } from '@/views'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen options={{headerShown:false}}name='Home' component={HomeScreen} />
-    </Stack.Navigator>
+    <BaseNavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name='Home' component={HomeScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='Login' component={LoginScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='SignUp' component={SignUpScreen} />
+        <Stack.Screen options={{ headerShown: false }} name='Welcome' component={WelcomeSreen} />
+      </Stack.Navigator>
+    </BaseNavigationContainer>
   );
 }
