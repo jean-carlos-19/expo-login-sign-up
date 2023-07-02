8d0d15ec9ce19a7be57cf51bfb214a88aedafc54
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-01 21:53:02 -0500

    Creado los archivos LoginScreen.tsx WelcomeScreen.tsx SignUpScreen.tsx

---
+++ src/views/SignUpScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUpScreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export {SignUpScreen}
---
+++ src/views/WelcomeSreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomeSreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export {WelcomeSreen}
