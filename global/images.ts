5117c5cb48aa1c1debe19b1086020f768e18550a
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-03 11:21:12 -0500

    Creadi kis archivos images.ts y routes.ts

---
+++ global/images.ts
@@ -0,0 +1,6 @@
+const IMAGES = Object.freeze({
+    "login":require('../assets/images/login.png'),
+    "signUp":require('../assets/images/signup.png'),
+    "welcome":require('../assets/images/welcome.png')
+})
+export {IMAGES}
