c47bcff904d4d67b04e6cd215df50808f48502ff
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-03 11:23:51 -0500

    Creado la pantalla Welcome

--- src/views/WelcomeSreen.tsx
+++ src/views/WelcomeSreen.tsx
@@ -1,11 +1,54 @@
 import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { theme } from '@/global/theme'
+import { IMAGES } from '@/global/images'
+import { useNavigation } from '@react-navigation/native'
 
 const WelcomeSreen = () => {
+  const navigation =  useNavigation();
   return (
-    <div>
-      
-    </div>
+    <View
+      className='flex-1'
+      style={{ backgroundColor: theme.bg }}
+    >
+      <View className="flex-1 flex justify-around my-4">
+
+        <Text className="text-white font-bold text-4xl text-center">
+          Let's Get Started!
+        </Text>
+
+        <View className="flex-row justify-center">
+          <Image
+            style={{ width: 350, height: 350 }}
+            source={IMAGES.welcome} />
+        </View>
+
+        <View className="space-y-4">
+          <TouchableOpacity
+            className='py-3 bg-yellow-400 mx-7 rounded-xl'
+            onPress={()=>{
+              navigation.navigate('SignUp')
+            }}
+          >
+            <Text
+              className='text-xl font-bold text-center text-gray-700'
+            >
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+          <View className='flex-row justify-center'>
+            <Text className="text-white font-semibold">Already have a account?</Text>
+            <TouchableOpacity onPress={()=>{
+              navigation.navigate('Login')
+            }}>
+              <Text className="font-semibold text-yellow-400"> Log In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        
+      </View>
+    </View>
   )
 }
 
-export {WelcomeSreen}
+export { WelcomeSreen }
